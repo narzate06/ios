5ddeaec2a33ed2041926e1295e5cbc64b5528806
Noe Arzate <narzate@gmail.com>
2016-09-23 15:44:23 -0500

    Checkpoint-19-completed

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Noe Arzate on 9/23/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alcolator/Alcolator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Alcolator
+//
+//  Created by Noe Arzate on 9/23/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
