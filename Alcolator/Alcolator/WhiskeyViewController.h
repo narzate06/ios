bf6df853caea96ff708b88f354fb19e8102bafdb
Noe Arzate <narzate@gmail.com>
2016-09-27 16:26:03 -0500

    checkpoint-20-storytocode:completed

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Noe Arzate on 9/27/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
